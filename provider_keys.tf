25d6cec52b142cfa9b9e44bbccb946bf313df405
srikanth <srikanth250690@gmail.com>
2023-09-24 14:16:27 +0530

    moving cred

--- provider_keys.tf
+++ provider_keys.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUJI23AZ6IBCWWHOB"
-  secret_key = "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"
+  shared_credentials_files = ["/root/.aws/credentials"]
 }
